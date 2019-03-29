bd9ca161a5c0a0619487576b54cc7cf66ab21b4c
Fernando G <fgomez@masterdevel.com>
2019-03-29 06:55:23 -0600

    add or usage

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 // Find patterns in strings using the Test method
 
-let testStr = "Hello, world";
-let testRegex = /Hello/;
+const testStr = "Hello, world";
+const testRegex = /Hello/;
 console.log(testRegex.test(testStr), /Hello/.test(testRegex));
 
+// Match multiple patterns using OR " | ".
+const animals = "cat, dog, cow, shark";
+const myAnimals = /chicken|hen|cow/;
+console.log(myAnimals.test(animals));
