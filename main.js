81ed0d3b1bd73bed778ec157663246b7cc0415c6
Fernando G <fgomez@masterdevel.com>
2019-04-01 06:21:47 -0600

    add g flag

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Find patterns in strings using the Test method
+// Find patterns in strings using the Test method.
 const testStr = "Hello, world";
 const testRegex = /Hello/;
 console.log(testRegex.test(testStr), /Hello/.test(testRegex));
@@ -8,12 +8,16 @@ const animals = "cat, dog, cow, shark";
 const myAnimals = /chicken|hen|cow/;
 console.log(myAnimals.test(animals));
 
-// Ignore casing using " i "
+// Ignore casing using " i ".
 const greetingStr = "Hi, my name is Fernando.";
 const nameRegex = /fernando/i;
 
 console.log(nameRegex.test(greetingStr));
 
-// Extract a word using Match 
+// Extract a word using Match.
 const myGreeting = "hello, world";
 console.log(myGreeting.match(/hello/));
+
+// Find more matches using the " g " flag.
+const myRepeat = "hello, hello, hello";
+console.log(myRepeat.match(/hello/g));
